af494b24920cee25136b9701c12b50f78a8ab33c
tomascabrerizo <tomascabrerizo97@gmail.com>
2021-01-19 18:00:14 -0300

    fix problem with vertex_count and tex_coords_count

--- code/mesh.cpp
+++ code/mesh.cpp
@@ -1,11 +1,16 @@
 #include <GL/glew.h>
 #include "mesh.h"
+#include <stdio.h>
 
-Mesh mesh_create(float* vertices, uint32_t v_count, float* tex_coords, uint32_t t_count)
+#define VERTEX_SIZE 12
+#define TEX_COORD_SIZE 8
+
+Mesh mesh_create(float* vertices, uint32_t vertices_size, float* tex_coords, uint32_t tex_coords_size)
 {
     Mesh new_mesh = {};
-    new_mesh.vertices_count = v_count;
-    new_mesh.tex_coords_count = t_count;
+    new_mesh.vertices_count = vertices_size / VERTEX_SIZE;
+    new_mesh.tex_coords_count = tex_coords_size / TEX_COORD_SIZE;
+    printf("%d\n",new_mesh.vertices_count);
     
     uint32_t vbo, tbo;
     glGenVertexArrays(1, &new_mesh.vao);
@@ -13,13 +18,13 @@ Mesh mesh_create(float* vertices, uint32_t v_count, float* tex_coords, uint32_t 
 
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, v_count, vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, vertices_size, vertices, GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
     glEnableVertexAttribArray(0);
 
     glGenBuffers(1, &tbo);
     glBindBuffer(GL_ARRAY_BUFFER, tbo);
-    glBufferData(GL_ARRAY_BUFFER, t_count, tex_coords, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, tex_coords_size, tex_coords, GL_STATIC_DRAW);
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 2* sizeof(float), 0);
     glEnableVertexAttribArray(1);
     
